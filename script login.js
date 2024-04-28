9d19c81b9732af8250da1d9d5f40e2da3dc59961
Alejandrolopez1989 <140138063+Alejandrolopez1989@users.noreply.github.com>
2024-04-28 16:40:11 -0400

    Update script login.js

--- script login.js
+++ script login.js
@@ -1,32 +1,33 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita el envío del formulario
-  
-    // Obtén el nombre de usuario y la contraseña ingresados
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    // Define un objeto o un array con los usuarios y contraseñas permitidos
-    var users = [
-      { username: "Keonex", password: "alejandro89" },
-      { username: "Guillermo", password: "maykol2023" },
-      { username: "yosbani", password: "yosbani2024cuba" },
-      { username: "Perrito18", password: "supercan" },
-      { username: "Dragón", password: "12345" },
-      { username: "Owen", password: "owendg06" },
-      { username: "Jumahite", password: "jumahite56" },    
-      { username: "Naylis", password: "Nefc01" }
-      
-    ];
-  
-    // Verifica si el usuario y la contraseña coinciden con los datos almacenados
-    for (var i = 0; i < users.length; i++) {
-      if (users[i].username === username && users[i].password === password) {
-        // Redirecciona a la página deseada si se encuentra una coincidencia
-        window.location.href = "inicio.html";
-        return;
-      }
+  event.preventDefault(); // Evita el envío del formulario
+
+  // Obtén el nombre de usuario y la contraseña ingresados
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Define un objeto o un array con los usuarios y contraseñas permitidos
+  var users = [
+    { username: "Keonex", password: "alejandro89" },
+    { username: "Guillermo", password: "maykol2023" },
+    { username: "yosbani", password: "yosbani2024cuba" },
+    { username: "Perrito18", password: "supercan" },
+    { username: "Dragón", password: "12345" },
+    { username: "Owen", password: "owendg06" },
+    { username: "Jumahite", password: "jumahite56" },
+    { username: "Carlos Santana", password: "Guau0215" },    
+    { username: "Naylis", password: "Nefc01" }
+    
+  ];
+
+  // Verifica si el usuario y la contraseña coinciden con los datos almacenados
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].username === username && users[i].password === password) {
+      // Redirecciona a la página deseada si se encuentra una coincidencia
+      window.location.href = "inicio.html";
+      return;
     }
-  
-    // Si no se encuentra una coincidencia, muestra un mensaje de error
-    alert("Nombre de usuario o contraseña inválidos. Por favor, inténtalo de nuevo.");
-  });
+  }
+
+  // Si no se encuentra una coincidencia, muestra un mensaje de error
+  alert("Nombre de usuario o contraseña inválidos. Por favor, inténtalo de nuevo.");
+});
