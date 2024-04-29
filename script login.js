e5ec71df3dc4ba07afdaff2c0e1732b72e4fa0e9
Alejandrolopez1989 <140138063+Alejandrolopez1989@users.noreply.github.com>
2024-04-29 09:03:49 -0400

    Update script login.js

--- script login.js
+++ script login.js
@@ -11,7 +11,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     { username: "Guillermo", password: "maykol2023" },
     { username: "yosbani", password: "yosbani2024cuba" },
     { username: "Perrito18", password: "supercan" },
-   // { username: "Dragón", password: "12345" },
+    { username: "Dragón", password: "12345" },
     { username: "Owen", password: "owendg06" },
     { username: "Jumahite", password: "jumahite56" },
     { username: "Carlos Santana", password: "Guau0215" },
