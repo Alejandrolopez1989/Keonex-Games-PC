133afed26012bb49add7a2f24ed65a38cef1d682
Alejandrolopez1989 <140138063+Alejandrolopez1989@users.noreply.github.com>
2024-04-29 20:56:41 -0400

    Actualizar script login.js

--- script login.js
+++ script login.js
@@ -21,6 +21,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     { username: "Miguel", password: "miguel" },
     { username: "Annabel", password: "manolito" },
     { username: "Anita", password: "2507" },
+    { username: "Madeline", password: "Cejesian*33" },
     { username: "Naylis", password: "Nefc01" }
     
   ];
